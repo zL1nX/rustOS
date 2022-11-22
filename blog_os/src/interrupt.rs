79bb41892be6df4cd84ab031e65137bb2c27d178
Max1z <linxzhan9@gmail.com>
2022-11-22 20:41:40 +0800

    post-07_处理timer这种interrupt (EOI)

--- blog_os/src/interrupt.rs
+++ blog_os/src/interrupt.rs
@@ -1,5 +1,6 @@
 
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use crate::print;
 use crate::println;
 use crate::gdt;
 use pic8259::ChainedPics;
@@ -12,6 +13,22 @@ pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8; // 给第一层那8个interrupt�
 pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe {ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)});
 // 自己重新定义个controller的offset, 用内置的chainedpics结构就能实现
 
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET, // 第一个中断, 所以没offset
+}
+
+impl InterruptIndex {
+    fn as_u8(self)->u8 {
+        self as u8
+    }
+
+    fn as_usize(self)->usize {
+        usize::from(self.as_u8())
+    }
+}
+
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
@@ -20,6 +37,7 @@ lazy_static! {
             idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
             // 设置double fault 的handler, 并且指定stack的index
         }
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler); // 给timer加上handler
         idt
     };
 }
@@ -36,6 +54,13 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
 
 extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code : u64) -> !{
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(stack_frame: InterruptStackFrame) {
+    print!(".");
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8()); // 显式地告知PIC我们的handler function 已经结束了
+    }
 }
 
 fn test_breakpoint_exception() {
