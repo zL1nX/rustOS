3271027407fea8b4e519c8b0a02d209d202e9552
XiaoLin <xiaolinzhang@stu.xidian.edu.cn>
2023-06-29 22:26:20 +0800

    post_11-fixed_size实现GlobalAlloc的两个必需函数

--- blog_os/src/allocator/fixed_size_block.rs
+++ blog_os/src/allocator/fixed_size_block.rs
@@ -1,5 +1,7 @@
-use alloc::alloc::Layout;
-use core::ptr;
+use alloc::alloc::{Layout, GlobalAlloc};
+use core::{mem, ptr::{self, NonNull}};
+
+use super::Locked;
 
 const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
 
@@ -29,7 +31,6 @@ impl FixedSizeBlockAllocator {
     }
 }
 
-
 // 自己的alloc,直接用别人的实现
 
 impl FixedSizeBlockAllocator {
@@ -39,9 +40,55 @@ impl FixedSizeBlockAllocator {
             Err(_) => ptr::null_mut()
         }
     }
+}
 
-    fn list_index(layout: &Layout) -> Option<usize> { // 根据给定的layout大小找到应该用哪个尺寸的block
-        let requested_size = layout.size().max(layout.align());
-        BLOCK_SIZES.iter().position(|&s| s >= requested_size)
+fn list_index(layout: &Layout) -> Option<usize> { // 根据给定的layout大小找到应该用哪个尺寸的block
+    let requested_size = layout.size().max(layout.align());
+    BLOCK_SIZES.iter().position(|&s| s >= requested_size)
+}
+
+unsafe impl GlobalAlloc for Locked<FixedSizeBlockAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut allocator = self.lock();
+        match list_index(&layout) {
+            Some(index) => {
+                match allocator.lists_heads[index].take() {
+                    Some(node) => { // 有合适的node list, 就返回这个list的头节点
+                        allocator.lists_heads[index] = node.next.take(); // 更新头节点
+                        node as *mut ListNode as *mut u8
+                    }
+                    None => { // 有合适的node list, 但是当前的block节点都被分配完了
+                        let block_size = BLOCK_SIZES[index];
+                        let block_align = block_size;
+                        let new_layout = Layout::from_size_align(block_size, block_align).unwrap();
+                        allocator.fallback_alloc(new_layout) // 那就没办法用fall back来alloc (根据block size重新来一个layout)
+                    }
+                }
+            }
+            None => allocator.fallback_alloc(layout), // 当前没有合适的block尺寸适合layout, 所以干脆就直接分配layout
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        let mut allocator = self.lock();
+        match list_index(&layout) {
+            Some(index) => {
+                let new_node = ListNode {
+                    next : allocator.lists_heads[index].take()
+                }; //新节点将直接在原来的block之前
+
+                // 确保大小正确
+                assert!(mem::size_of::<ListNode>() <= BLOCK_SIZES[index]);
+                assert!(mem::align_of::<ListNode>() <= BLOCK_SIZES[index]);
+
+                let new_node_ptr = ptr as *mut ListNode;
+                new_node_ptr.write(new_node);
+                allocator.lists_heads[index] = Some(&mut *new_node_ptr); // 解引用更新头节点
+            }
+            None => {
+                let node_ptr = NonNull::new(ptr).unwrap(); // 适应下面函数的需求
+                allocator.fallback_allocator.deallocate(node_ptr, layout);
+            }
+        }
     }
 }
